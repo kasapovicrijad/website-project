f9110a82301a4ab51bf7df4424aa960a23aaedcf
kasapovicrijad <rikacblyat@gmail.com>
2024-06-08 17:49:02 +0200

    practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
-import './App.css';
+import React, { useState } from 'react';
+import TodoList from './components/TodoList';
+import TodoForm from './components/TodoForm';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    setTodos([...todos, { text: todo, completed: false }]);
+  };
+
+  const toggleComplete = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].completed = !newTodos[index].completed;
+    setTodos(newTodos);
+  };
+
+  const removeTodo = (index) => {
+    const newTodos = todos.filter((_, i) => i !== index);
+    setTodos(newTodos);
+  };
+
   return (
-    <div className="App">
-    <h1>my app</h1>
+    <div className="container mx-auto mt-8">
+      <h1 className="text-2xl font-bold text-center">To-Do List</h1>
+      <TodoForm addTodo={addTodo} />
+      <TodoList todos={todos} toggleComplete={toggleComplete} removeTodo={removeTodo} />
     </div>
   );
 }
